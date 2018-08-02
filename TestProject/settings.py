9ee0d5e3afe02083bc3dece70d62ab95151ee4a2
Chintan Vaghela <cheaterchintz@gmail.com>
2018-08-01 22:41:07 -0400

    url update

--- TestProject/settings.py
+++ TestProject/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = 'TestProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR ,'template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- mytestapp/views.py
+++ mytestapp/views.py
@@ -4,9 +4,5 @@ from __future__ import unicode_literals
 # Create your views here.
 from django.shortcuts import render
 
-
-from django.http import HttpResponse
-
 def hello(request):
-   text = """<h1>Welcome to My Test App!</h1>"""
-   return HttpResponse(text)
+   return render(request, "hello.html", {})
